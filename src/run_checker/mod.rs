dede08981e970c335f8b1616a1a4b2ab8f0e9685
zjp <jiping_zhou@foxmail.com>
2024-07-22 22:56:25 +0800

    os-checker: add run_checker::Output; snapshot test on test suite

--- src/run_checker/mod.rs
+++ src/run_checker/mod.rs
@@ -1,9 +1,12 @@
 use crate::{
     layout::Layout,
-    repo::{Config, Resolve},
+    repo::{CheckerTool, Config, Resolve},
     Result,
 };
+use cargo_metadata::{camino::Utf8PathBuf, Message};
 use eyre::Context;
+use serde::Deserialize;
+use std::process::Output as RawOutput;
 
 #[derive(Debug)]
 pub struct Repo {
@@ -23,6 +26,33 @@ impl Repo {
     }
 }
 
+pub struct Output {
+    raw: RawOutput,
+    parsed: OutputParsed,
+}
+
+#[derive(Debug)]
+pub enum OutputParsed {
+    Fmt(Box<[FmtMessage]>),
+    Clippy(Box<[Message]>),
+}
+
+#[derive(Debug, Deserialize)]
+pub struct FmtMessage {
+    name: Utf8PathBuf,
+    mismatches: Box<[FmtMismatch]>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct FmtMismatch {
+    original_begin_line: u32,
+    original_end_line: u32,
+    expected_begin_line: u32,
+    expected_end_line: u32,
+    original: Box<str>,
+    expected: Box<str>,
+}
+
 #[test]
 fn repo() -> Result<()> {
     let yaml = "
@@ -30,9 +60,14 @@ arceos:
   all: true
   miri: false
 ";
-    let repo = &Repo::new("tmp/test-fmt", &[], Config::from_yaml(yaml)?.pop().unwrap())?;
-    // let repo = &Repo::new("repos/arceos", &[], Config::from_yaml(yaml)?.pop().unwrap())?;
+    let repo = Repo::new(
+        "repos/os-checker-test-suite",
+        &[],
+        Config::from_yaml(yaml)?.pop().unwrap(),
+    )?;
+    // let repo = Repo::new("repos/arceos", &[], Config::from_yaml(yaml)?.pop().unwrap())?;
     let resolve = repo.resolve()?;
+    let mut snapshot = Vec::with_capacity(resolve.len());
     for res in resolve.iter().take(4) {
         let out = res
             .expr
@@ -40,13 +75,32 @@ arceos:
             .stdout_capture()
             .unchecked()
             .run()?;
+
+        let stdout: &[_] = &out.stdout;
+        let parsed = match res.checker {
+            CheckerTool::Fmt => OutputParsed::Fmt(serde_json::from_slice(stdout)?),
+            CheckerTool::Clippy => OutputParsed::Clippy(
+                Message::parse_stream(stdout)
+                    .map(|mes| mes.with_context(|| "解析 Clippy Json 输出失败"))
+                    .collect::<Result<_>>()?,
+            ),
+            CheckerTool::Miri => todo!(),
+            CheckerTool::SemverChecks => todo!(),
+            CheckerTool::Lockbud => todo!(),
+        };
+
         let stdout = std::str::from_utf8(&out.stdout)?;
         let stderr = std::str::from_utf8(&out.stderr)?;
-        println!(
-            "[{} with {:?} checking] exit status{}\nstdout={stdout}\nstderr={stderr}",
+
+        snapshot.push(format!(
+            "[{} with {:?} checking] {}\nstdout={stdout}\nstderr={stderr}\nparsed={parsed:?}",
             res.package.name, res.checker, out.status
-        );
+        ));
     }
+
+    let current_path = Utf8PathBuf::from(".").canonicalize_utf8()?;
+    let join = snapshot.join("\n\n").replace(current_path.as_str(), ".");
+    expect_test::expect_file!["./tests.snapshot"].assert_eq(&join);
 
     Ok(())
 }
