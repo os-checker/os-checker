7a1c324b2a9ac1d184187315bf7cd60d2d5c8930
zjp <jiping_zhou@foxmail.com>
2024-08-05 13:10:12 +0800

    feat(cli): --emit a.json 现在会另外生成包含原始输出的 a_raw_reports.json

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -5,7 +5,10 @@ use crate::{
 };
 use argh::FromArgs;
 use cargo_metadata::camino::Utf8PathBuf;
+use eyre::ContextCompat;
+use itertools::Itertools;
 use rayon::prelude::*;
+use std::fs::File;
 
 pub fn args() -> Args {
     let arguments = argh::from_env();
@@ -73,12 +76,28 @@ impl Args {
                 }
             }
             Emit::Json => {
-                let (tree, _raw_reports) = json_treenode(&stats);
+                let (tree, raw_reports) = json_treenode(&stats);
                 serde_json::to_writer(std::io::stdout(), &tree)?;
+                let reports = raw_reports
+                    .iter()
+                    .map(|(key, r)| (*key, r.to_serialization()))
+                    .collect_vec();
+                serde_json::to_writer(std::io::stdout(), &reports)?;
             }
             Emit::JsonFile(p) => {
                 let (tree, raw_reports) = json_treenode(&stats);
-                serde_json::to_writer(std::fs::File::create(p)?, &tree)?;
+                serde_json::to_writer(File::create(p)?, &tree)?;
+                let file_stem = p
+                    .file_stem()
+                    .with_context(|| format!("{p} doesn't contain the file name"))?;
+                let report_path = p
+                    .clone()
+                    .with_file_name(format!("{file_stem}_raw_reports.json"));
+                let reports = raw_reports
+                    .iter()
+                    .map(|(key, r)| (*key, r.to_serialization()))
+                    .collect_vec();
+                serde_json::to_writer(File::create(&*report_path)?, &reports)?;
             }
         }
         debug!(?self.emit, "Output emitted");
