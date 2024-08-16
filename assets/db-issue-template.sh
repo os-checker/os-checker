451ab6687fb7e5ef4c300cae8c220550033c8973
zjp <jiping_zhou@foxmail.com>
2024-08-16 15:00:40 +0800

    chore(jq): 原始输出计数与 count 应该一致 ref https://github.com/os-checker/database/issues/4

--- assets/db-issue-template.sh
+++ assets/db-issue-template.sh
@@ -1,11 +1,16 @@
 repo=os-checker/database
 db_issue=tmp/db-issue.txt
 
-q=". as \$x | .idx |= map(select(.count!=0)|{package,tool,count,duration_ms}) | {
-  pkg: .idx | unique_by(.package) | map(\$x.env.packages[.package] + {package:.package}), # 只选有错误的 package
-  idx,
-  data: .data | map(del(.raw)), # 去除原输出
-}
+q=". as \$x | .data | group_by(.idx) | map({
+  cmd_idx: .[0].idx,
+  length: . | length,
+} | . + { cmd: \$x.idx[.cmd_idx] | {package, tool,count, duration_ms} } # 为了简洁，这里去掉一些字段
+  | . + { package: \$x.env.packages[.cmd.package] }
+  | . + { repo: \$x.env.repos[.package.repo.idx] }
+)
+"
+
+q_test="$q  | map(select(.length != .cmd.count)) # 如果为 []，则表示所有输出与 count 是一致的
 "
 
 cat >$db_issue <<EOF
@@ -17,14 +22,29 @@ $q\`\`\`
 <summary>jq 执行的 JSON 结果</summary>
 
 \`\`\`json
-$(cat tmp/test.json | jq "$q" | jsonxf | tee -a $db_issue)
+$(cat tmp/test.json | jq "$q" | jsonxf)
+\`\`\`
+
+</details>
+
+---
+
+\`\`\`jq
+$q_test\`\`\`
+
+<details>
+
+<summary>jq 测试</summary>
+
+\`\`\`json
+$(cat tmp/test.json | jq "$q_test" | jsonxf)
 \`\`\`
 
 </details>
 EOF
 
 # 提交 issue 到 database 仓库
-now=$(TZ=Asia/Shanghai date +"%Y-%m-%d %H:%M:%S")
-title="[$now] os-checker JSON 输出内容"
+# now=$(TZ=Asia/Shanghai date +"%Y-%m-%d %H:%M:%S")
+title="[test] os-checker JSON 原始输出计数与 count 应该一致"
 echo "$title"
 # gh issue create -R $repo --title "$title" --body-file $db_issue
