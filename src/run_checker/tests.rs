6635470450ad9f9c38f2493a7061d000637ba1b5
zjp <jiping_zhou@foxmail.com>
2024-08-16 17:20:35 +0800

    chore(test): 利用 jq 测试 JSON 原始输出计数与 count 应该一致
    close https://github.com/os-checker/database/issues/4

--- src/run_checker/tests.rs
+++ src/run_checker/tests.rs
@@ -18,7 +18,13 @@ file://repos/os-checker-test-suite:
 
     let mut json = JsonOutput::new();
     output.with_json_output(&mut json);
-    expect_file!["./snapshots/json-test-suite.txt"].assert_eq(&to_string_pretty(&json)?);
+
+    let json_str = to_string_pretty(&json)?;
+    expect_file!["./snapshots/json-test-suite.txt"].assert_eq(&json_str);
+
+    let json_bytes = json_str.as_bytes();
+    let json_count = jq_count(json_bytes)?;
+    expect_file!["./snapshots/json-test-suite_jq_count.txt"].assert_eq(&json_count);
 
     Ok(())
 }
@@ -40,39 +46,35 @@ file://repos/arceos:
     Ok(())
 }
 
-// #[test]
-// fn local_and_github() -> Result<()> {
-//     use rayon::prelude::*;
-//
-//     // 该测试只写入日志文件
-//     fn logging(configs: Vec<Config>) -> Result<()> {
-//         debug!(?configs);
-//         let repos: Vec<_> = configs
-//             .into_par_iter()
-//             .map(Repo::try_from)
-//             .collect::<Result<_>>()?;
-//         for repo in &repos {
-//             trace!(?repo);
-//             let stat = repo.outputs_and_statistics()?;
-//             for s in stat.iter().filter(|s| !s.check_fine()) {
-//                 let count_on_file = s.table_of_count_of_file();
-//                 let count_on_kind = s.table_of_count_of_kind();
-//                 info!("\n{count_on_file}\n{count_on_kind}");
-//             }
-//         }
-//         Ok(())
-//     }
-//
-//     crate::logger::test_init("assets/run_checker-github.log");
-//
-//     let yaml = std::fs::read_to_string("assets/repos.yaml")?;
-//     logging(Config::from_yaml(&yaml)?)?;
-//
-//     let yaml = "
-// os-checker/os-checker-test-suite:
-//   all: true
-// ";
-//     logging(Config::from_yaml(yaml)?)?;
-//
-//     Ok(())
-// }
+fn jq_count(json_bytes: &[u8]) -> Result<String> {
+    let query = "
+. as $x | .data | group_by(.idx) | map({
+  cmd_idx: .[0].idx,
+  length: . | length,
+} | . + { cmd: $x.idx[.cmd_idx] | {package, tool,count, duration_ms} } # 为了简洁，这里去掉一些字段
+  | . + { package: $x.env.packages[.cmd.package] }
+  | . + { repo: $x.env.repos[.package.repo.idx] }
+)
+";
+    let out1 = duct::cmd!("jq", query)
+        .stdin_bytes(json_bytes)
+        .stdout_capture()
+        .run()?;
+
+    // 若结果为 []，则表示所有输出与 count 是一致的
+    let out2 = duct::cmd!(
+        "jq",
+        format!("{query} | map(select(.length != .cmd.count))")
+    )
+    .stdin_bytes(json_bytes)
+    .stdout_capture()
+    .run()?;
+
+    let diff: serde_json::Value = serde_json::from_slice(&out2.stdout)?;
+    ensure!(
+        diff.as_array().map(|arr| arr.is_empty()).unwrap_or(false),
+        "输出统计与 count 数值不一致，差异为：\n{diff:?}"
+    );
+
+    Ok(String::from_utf8(out1.stdout)?)
+}
