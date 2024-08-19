835aece8c84a172ee8c424c672c47f0fb29a249f
zjp <jiping_zhou@foxmail.com>
2024-08-19 22:26:59 +0800

    chore(test): add cargo_check_verbose

--- src/layout/tests.rs
+++ src/layout/tests.rs
@@ -11,3 +11,27 @@ fn arceos_layout() {
     expect_file!["./snapshots/arceos.txt"].assert_debug_eq(&arceos);
     expect_file!["./snapshots/arceos-packages.txt"].assert_debug_eq(&arceos.packages());
 }
+
+#[test]
+fn cargo_check_verbose() -> crate::Result<()> {
+    use itertools::Itertools;
+    let re = regex::Regex::new(r#"^\s+Running `CARGO="#)?;
+    let reader = duct::cmd!("cargo", "check", "-vv")
+        .stderr_to_stdout()
+        .reader()?;
+    let messages: Vec<_> = cargo_metadata::Message::parse_stream(std::io::BufReader::new(reader))
+        .filter_map(|parsed| {
+            parsed.ok().and_then(|line| {
+                if let cargo_metadata::Message::TextLine(line) = line {
+                    if re.is_match(&line) {
+                        return Some(line);
+                    }
+                }
+                None
+            })
+        })
+        .dedup()
+        .collect();
+    expect_file!["./snapshots/cargo-check-verbose.txt"].assert_debug_eq(&messages);
+    Ok(())
+}
