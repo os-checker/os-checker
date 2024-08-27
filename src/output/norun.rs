59c66d2519dc8a9c34b422f99fbf71dbc5a6dee3
zjp <jiping_zhou@foxmail.com>
2024-08-27 15:10:25 +0800

    feat(setup): install detected targets for toolchain required by lockbud

--- src/output/norun.rs
+++ src/output/norun.rs
@@ -1,5 +1,6 @@
 use super::RustToolchains;
 use crate::Result;
+use duct::cmd;
 use indexmap::IndexSet;
 use itertools::Itertools;
 use serde::Serialize;
@@ -31,8 +32,15 @@ impl Norun {
     }
 
     pub fn setup(&self) -> Result<()> {
-        let list = self.targets.iter().map(|s| s.as_str());
-        duct::cmd("rustup", ["target", "add"].into_iter().chain(list)).run()?;
+        let list = self.targets.iter().map(|s| s.as_str()).collect_vec();
+
+        // install detected targets for host toolchain
+        rustup_target_add(&list).run()?;
+
+        // install detected targets for toolchain required by lockbud
+        setup_lockbud(&list)?;
+
+        // install toolchains required by all repos
         self.toolchains.setup()
     }
 }
@@ -41,4 +49,17 @@ fn update_set(set: &mut IndexSet<String>, val: &str) {
     if set.get(val).is_none() {
         set.insert(val.to_owned());
     }
+}
+
+fn rustup_target_add(targets: &[&str]) -> duct::Expression {
+    cmd("rustup", ["target", "add"].iter().chain(targets))
+}
+
+fn setup_lockbud(targets: &[&str]) -> Result<()> {
+    let url = "https://github.com/BurtonQin/lockbud.git";
+    let dir = "repos/lockbud";
+    cmd!("git", "clone", url, dir).run()?;
+    rustup_target_add(targets).dir(dir).run()?;
+    cmd!("rustup", "show").run()?;
+    Ok(())
 }
