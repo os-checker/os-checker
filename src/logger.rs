0161e9bb60a44e2e61255ee55e3de910aaf14374
zjp <jiping_zhou@foxmail.com>
2024-09-09 21:36:49 +0800

    chore(logger): 暂停记录 ErrorLayer，输出太乱了
    2024-09-09T13:30:30.347794Z DEBUG execute{self=Args { sub_args: Run(ArgsRun { config: ["repos.json"], emit: Json, clean_repo: true }) }}:execute{self=ArgsRun { config: ["repos.json"], emit: Json, clean_repo: true }}:try_from{config=Config { uri: Github("kern-crates/rCoreloongArch"), config: RepoConfig { setup: None, targets: None, cmds: Cmds { map: {} }, packages: {} } }}:run_check{self=Repo { layout: Layout { repo_root: "repos/kern-crates/rCoreloongArch", cargo_tomls: ["repos/kern-crates/rCoreloongArch/Cargo.toml", "repos/kern-crates/rCoreloongArch/easy-fs/Cargo.toml", "repos/kern-crates/rCoreloongArch/easy-fs-fuse/Cargo.toml", "repos/kern-crates/rCoreloongArch/isomorphic_drivers/Cargo.toml", "repos/kern-crates/rCoreloongArch/kernel/Cargo.toml", "repos/kern-crates/rCoreloongArch/pci/Cargo.toml", "repos/kern-crates/rCor
    2024-09-09T13:30:30.348036Z DEBUG execute{self=Args { sub_args: Run(ArgsRun { config: ["repos.json"], emit: Json, clean_repo: true }) }}:execute{self=ArgsRun { config: ["repos.json"], emit: Json, clean_repo: true }}:try_from{config=Config { uri: Github("kern-crates/rCoreloongArch"), config: RepoConfig { setup: None, targets: None, cmds: Cmds { map: {} }, packages: {} } }}:run_check{self=Repo { layout: Layout { repo_root: "repos/kern-crates/rCoreloongArch", cargo_tomls: ["repos/kern-crates/rCoreloongArch/Cargo.toml", "repos/kern-crates/rCoreloongArch/easy-fs/Cargo.toml", "repos/kern-crates/rCoreloongArch/easy-fs-fuse/Cargo.toml", "repos/kern-crates/rCoreloongArch/isomorphic_drivers/Cargo.toml", "repos/kern-crates/rCoreloongArch/kernel/Cargo.toml", "repos/kern-crates/rCoreloongArch/pci/Cargo.toml", "repos/kern-crates/rCor
    ...

--- src/logger.rs
+++ src/logger.rs
@@ -1,15 +1,14 @@
-use tracing_error::ErrorLayer;
 use tracing_subscriber::{fmt, prelude::*, registry, EnvFilter};
 
 pub fn init() {
     let fmt_layer = fmt::layer();
     let env_layer = EnvFilter::from_default_env();
-    let error_layer = ErrorLayer::default();
+    // let error_layer = tracing_error::ErrorLayer::default();
 
     if let Err(err) = registry()
         .with(fmt_layer)
         .with(env_layer)
-        .with(error_layer)
+        // .with(error_layer)
         .try_init()
     {
         eprintln!("Logger already init: {err}");
