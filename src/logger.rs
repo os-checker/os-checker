c1ed3220edaebb5de27b9eff96144bd725dce2a6
zjp <jiping_zhou@foxmail.com>
2024-09-10 14:44:11 +0800

    fix(color_eyre): 恢复 tracing_error::ErrorLayer
    它不影响 tracing 的日志输出。
    
    Warning: SpanTrace capture is Unsupported.
    Ensure that you've setup a tracing-error ErrorLayer and the semver versions are compatible

--- src/logger.rs
+++ src/logger.rs
@@ -3,12 +3,12 @@ use tracing_subscriber::{fmt, prelude::*, registry, EnvFilter};
 pub fn init() {
     let fmt_layer = fmt::layer();
     let env_layer = EnvFilter::from_default_env();
-    // let error_layer = tracing_error::ErrorLayer::default();
+    let error_layer = tracing_error::ErrorLayer::default();
 
     if let Err(err) = registry()
         .with(fmt_layer)
         .with(env_layer)
-        // .with(error_layer)
+        .with(error_layer)
         .try_init()
     {
         eprintln!("Logger already init: {err}");
