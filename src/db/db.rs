66e793164f61f2e28a42e6c294759ede3f53e2af
zjp <jiping_zhou@foxmail.com>
2024-09-27 20:57:44 +0800

    chore(db): add Db::{write, set, get}

--- src/db/db.rs
+++ src/db/db.rs
@@ -2,7 +2,7 @@ use super::{CacheRepoKey, CacheRepoValue};
 use crate::Result;
 use camino::Utf8Path;
 use eyre::Context;
-use redb::{Database, TableDefinition};
+use redb::{Database, Table, TableDefinition};
 use std::sync::Arc;
 
 const TABLE: TableDefinition<CacheRepoKey, CacheRepoValue> = TableDefinition::new("data");
@@ -20,21 +20,40 @@ impl Db {
         Ok(Db { db })
     }
 
+    pub fn get(&self, key: &CacheRepoKey) -> Result<Option<CacheRepoValue>> {
+        let table = self.db.begin_read()?.open_table(TABLE)?;
+        Ok(table.get(key)?.map(|guard| guard.value()))
+    }
+
+    fn write(
+        &self,
+        f: impl for<'a> FnOnce(&mut Table<'a, CacheRepoKey, CacheRepoValue>) -> Result<()>,
+    ) -> Result<()> {
+        let write_txn = self.db.begin_write()?;
+        f(&mut write_txn.open_table(TABLE)?)?;
+        write_txn.commit()?;
+        Ok(())
+    }
+
+    pub fn set(&self, key: &CacheRepoKey, value: CacheRepoValue) -> Result<()> {
+        self.write(|table| {
+            table.insert(key, value)?;
+            Ok(())
+        })
+    }
+
     pub fn set_or_replace(
         &self,
         key: &CacheRepoKey,
         f: impl FnOnce(Option<CacheRepoValue>) -> Result<CacheRepoValue>,
     ) -> Result<()> {
-        let write_txn = self.db.begin_write()?;
-        {
-            let mut table = write_txn.open_table(TABLE)?;
+        self.write(|table| {
             let opt_value = table.remove(key)?.map(|guard| guard.value());
             let mut value = f(opt_value)?;
             value.update_unix_timestamp();
             table.insert(key, value)?;
-        }
-        write_txn.commit()?;
-        Ok(())
+            Ok(())
+        })
     }
 }
 
