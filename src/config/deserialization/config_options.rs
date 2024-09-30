39b784b7caa285131f6226dbb81838da68080970
zjp <jiping_zhou@foxmail.com>
2024-10-01 07:44:44 +0800

    chore(mirai): 暂不运行 mirai；仅测试 repos.json

--- src/config/deserialization/config_options.rs
+++ src/config/deserialization/config_options.rs
@@ -118,14 +118,14 @@ impl Cmds {
                 Fmt => ENABLED,
                 Clippy => ENABLED,
                 Lockbud => ENABLED,
-                Mirai => ENABLED,
+                // Mirai => ENABLED,
             },
         }
     }
 
     /// TODO: 其他工具待完成
     pub fn enable_all_checkers(&mut self) {
-        for checker in [Fmt, Clippy, Lockbud, Mirai] {
+        for checker in [Fmt, Clippy, Lockbud] {
             self.entry(checker)
                 .and_modify(|cmd| *cmd = ENABLED)
                 .or_insert(ENABLED);
