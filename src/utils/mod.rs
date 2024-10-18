0097d5ad2672e3a72be67bf308799196aba3a2b7
zjp <jiping_zhou@foxmail.com>
2024-10-18 10:46:25 +0800

    feat(rap): install toolchain

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -25,6 +25,9 @@ pub const PLUS_TOOLCHAIN_LOCKBUD: &str = "+nightly-2024-05-21";
 pub const TOOLCHAIN_MIRAI: &str = "nightly-2023-12-30";
 pub const PLUS_TOOLCHAIN_MIRAI: &str = "+nightly-2023-12-30";
 
+pub const TOOLCHAIN_RAP: &str = "nightly-2024-06-30";
+pub const PLUS_TOOLCHAIN_RAP: &str = "+nightly-2024-06-30";
+
 /// git clone 一个仓库到一个 dir。
 /// 如果该仓库已存在，则 git pull 拉取最新的代码。
 #[instrument(level = "trace")]
