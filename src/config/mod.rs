c6b6ae22535f03b84c91b45024431d8367178f19
zjp <jiping_zhou@foxmail.com>
2024-10-29 09:22:23 +0800

    chore(config): downgrade debug!(?config)

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -204,7 +204,7 @@ impl TryFrom<Value> for Configs {
                         RepoConfig::deserialize(deserializer).with_context(|| PARSE_JSON_ERROR)?;
                     config.validate_checker_name(&repo)?;
                     config.validate_skip_pkg_dir_globs(&repo)?;
-                    info!(?config);
+                    debug!(?config);
                     Ok(Config {
                         uri: uri::uri(repo)?,
                         config: Box::new(config),
